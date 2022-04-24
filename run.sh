6f1a43cfb2c7e88d59e2a26fd7f699eaf3efe0bd
CGH31415 <43187692+CGH31415@users.noreply.github.com>
2022-04-23 22:04:31 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,6 +5,7 @@ dataset=CIFAR100
 # data_dir='drive/MyDrive/Colab_Notebooks/'
 data_dir='../'
 save_dir='drive/MyDrive/Colab_Notebooks/test_save_dir'
+epochs = 5
 
-echo "python -u experiment.py  --arch=$model  --data=$dataset --data_dir=$data_dir  --save-dir=$save_dir"
-python -u experiment.py  --arch=$model  --data=$dataset --data_dir=$data_dir  --save-dir=$save_dir
+echo "python -u experiment.py  --arch=$model  --data=$dataset --data_dir=$data_dir  --save-dir=$save_dir --epochs=$epochs"
+python -u experiment.py  --arch=$model  --data=$dataset --data_dir=$data_dir  --save-dir=$save_dir --epochs=$epochs
