a7f9e22607becf336285d9be98c1814936b1f0ae
CGH31415 <43187692+CGH31415@users.noreply.github.com>
2022-04-26 17:09:41 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,7 +5,12 @@ dataset=CIFAR100
 # data_dir='drive/MyDrive/Colab_Notebooks/'
 data_dir='../'
 save_dir='drive/MyDrive/Colab_Notebooks/test_save_dir'
-epochs = 5
+epochs=5
+logname1='noteacher_01'
+logname2='noteacher_05'
 
-echo "python -u experiment.py  --arch=$model  --data=$dataset --data_dir=$data_dir  --save-dir=$save_dir --epochs=$epochs"
-python -u experiment.py  --arch=$model  --data=$dataset --data_dir=$data_dir  --save-dir=$save_dir --epochs=$epochs
+echo "python -u experiment.py  --arch=$model  --data=$dataset --data_dir=$data_dir  --save-dir=$save_dir/$logname1 --epochs=$epochs --log-name=$logname1"
+python -u experiment.py  --arch=$model  --data=$dataset --data_dir=$data_dir  --save-dir=$save_dir/$logname1 --epochs=$epochs --log-name=$logname1
+
+echo "python -u experiment.py  --arch=$model  --data=$dataset --data_dir=$data_dir  --save-dir=$save_dir/$logname2 --epochs=$epochs --log-name=$logname2 --lr=0.5"
+python -u experiment.py  --arch=$model  --data=$dataset --data_dir=$data_dir  --save-dir=$save_dir/$logname2 --epochs=$epochs --log-name=$logname2 --lr=0.5
